45df2ae4a7acd03ecd24c060b89568de656a1cb3
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-13 12:44:56 +1100

    make fraser-suzuki model function

--- fs_model.R
+++ fs_model.R
@@ -1,12 +1,26 @@
+#' Produce pca plot and loadings table
+#'
+#' @param dataframe dataframe of traits
+#' @param params parameter vector of starting values for h, s, p, and w
+#' @param lb lower bound
+#' @param ub upper bound
+#' @return saved pca plot and loadings table
+#' @importFrom minpack.lm nlsLM
+#'
+#' @export
+
 
 # function to do the nls fit with the correct starting values
 fs_model <- function (dataframe, params, lb, ub) {
 
   nlsLM(deriv ~ fs_function(temp_C, h, s, p, w),
-        start = list(h = params[1], s = params[2], p = params[3],
+        start = list(h = params[1], 
+                     s = params[2], 
+                     p = params[3],
                      w = params[4]),
         data = dataframe,
-        control = nls.lm.control(maxiter = 1024, maxfev = 1e6),
+        control = nls.lm.control(maxiter = 1024, 
+                                 maxfev = 1e6),
         lower = lb,
         upper = ub)
 
