b4c7822f7900578736d4c0e2e4353092cdc0cf65
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-13 15:01:22 +1100

    add function to load and process raw tga -- split off from the single_deconvolve function so can be used for the other files

---
+++ R/process_raw_tga.R
@@ -0,0 +1,18 @@
+#' Load and process raw TGA data
+#'
+#' @param raw_file TGA raw data
+#' @return processed TGA data
+#' @importFrom deconvolve process
+#'
+#' @export
+
+process_raw_tga <- function(raw_file) {
+  
+  df <- read.csv(raw_file, header = FALSE, skip = 29)
+  names(df) <- c('temp', 'time', 'mass_loss')
+  init_mass <- read.csv(raw_file, nrwos = 1, header = FALSE, skip = 17)[1,2]
+  munge <- deconvolve::process(df, 'temp', 'mass_loss', init_mass)
+  
+  munge
+  
+}
