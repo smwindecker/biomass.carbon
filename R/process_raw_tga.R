c2da1ab93a65a3326743f7d75fbe76b02ae9e49a
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-14 13:32:49 +1100

    fix typo

--- R/process_raw_tga.R
+++ R/process_raw_tga.R
@@ -10,7 +10,7 @@ process_raw_tga <- function(raw_file) {
   
   df <- read.csv(raw_file, header = FALSE, skip = 29)
   names(df) <- c('temp', 'time', 'mass_loss')
-  init_mass <- read.csv(raw_file, nrwos = 1, header = FALSE, skip = 17)[1,2]
+  init_mass <- read.csv(raw_file, nrows = 1, header = FALSE, skip = 17)[1,2]
   munge <- deconvolve::process(df, 'temp', 'mass_loss', init_mass)
   
   munge
