90262b8f4feccbe59bc809e26b513408f32fae18
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-31 19:30:52 +1100

    combine prep traits and analysis scripts

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,8 +1,6 @@
 
 # ALL TRAITS
 
-library(plyr)
-
 # load raw species data
 species <- read.csv('data-raw/species.csv', header = T)
 
@@ -44,3 +42,39 @@ t_mean$HC <- t_mean$HC_1 + t_mean$HC_2
 t_mean$HC[is.na(t_mean$HC)] <- t_mean$HC_2[is.na(t_mean$HC)]
 write.table(t_mean, 'data/all_traits.txt')
 
+
+library(dplyr)
+
+t_mean <- read.table('raw/all_traits.txt')
+phylogeny(t_mean, 
+          genbank_accessions_file = 'raw/GenBankAccessions.txt', 
+          nwk_file = 'raw/phylo_tree.nwk', 
+          output_folder = 'docs/')
+
+# prep log traits for pairplot and pca
+cov <- t_mean %>%
+  set_rownames(t_mean[, 'sp_abrev']) %>%
+  select(SLA, DMC, N, C, HC, CL, LG)
+boxplot(cov, output_folder = 'figs/')
+
+cov[] <- sapply(cov, log)
+
+pca_gf(cov, 
+       figure_folder = 'output/',
+       table_folder = 'docs/')
+
+pair_plot(cov, 
+          output_file = 'figs/pairplot.png')
+
+# Fraser-Suzuki parameter simulation
+fs_simulate(output_folder = 'figs/')
+
+######################
+single_deconvolve(raw_file = 'raw/raw_biomass/CL.csv', 
+                  subfig = 'a', 
+                  output_file = 'figs/raw_CL.png')
+single_deconvolve(raw_file = 'raw/raw_biomass/CL.csv', 
+                  subfig = 'b', 
+                  output_file = 'figs/raw_LG.png')
+
+species_df <- read.csv('raw/species.csv', header = TRUE)
