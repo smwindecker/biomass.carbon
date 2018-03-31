dfd59beb7c6dbe20edd3795f560e9e64f9aa4e26
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-31 19:10:39 +1100

    commend function

--- R/tga_param_table.R
+++ R/tga_param_table.R
@@ -1,30 +1,39 @@
 #' Produce parameter values table
 #'
 #' @param species_deconvolved_list list of deconvolved 
-#' @param species_df dataframe with species info
+#' @param species_data dataframe with species info
 #' @param output_folder folder path for output plot
 #' @return saved parameters tables
 #' @importFrom xtable xtable
 #'
 #' @export
 
-tga_param_table <- function(species_deconvolved_list, species_df, output_folder) {
+tga_param_table <- function(species_deconvolved_list, species_data, output_folder) {
   
+  # bind parameter outputs of species_deconvolved function
   parameter_estimates <- dplyr::bind_rows(lapply(1:length(species_deconvolved_list), function(x) {
     return(species_deconvolved_list[[x]]$params)
   }))
-  write.table(parameter_estimates, 'munge/tga_parameters.txt')
   
+  # save parameter output
+  write.table(parameter_estimates, 'data/tga_parameters.txt')
+  
+  # set significant digits
   parameter_estimates[, c('h1', 'h2', 'h3', 'h0')] <- signif(parameter_estimates[, c('h1', 'h2', 'h3', 'h0')], 4)
   parameter_estimates[, c('p1', 'p2', 'p3', 'p0')] <- signif(parameter_estimates[, c('p1', 'p2', 'p3', 'p0')], 3)
   parameter_estimates[, c('s1', 's2', 's3', 's0')] <- signif(parameter_estimates[, c('s1', 's2', 's3', 's0')], 2)
   parameter_estimates[, c('w1', 'w2', 'w3', 'w0')] <- signif(parameter_estimates[, c('w1', 'w2', 'w3', 'w0')], 2)
   
+  # combine with species data
   parameters <- merge(parameter_estimates, species_df[,c('species_code', 'species')])
   
+  # add italics latex code
   parameters$species <- paste0('\\textit{', parameters$species, '}')
+  
+  # produce xtable
   parameters <- xtable::xtable(parameters)
   
+  # save as .tex file
   print(parameters,
         include.rownames = FALSE,
         include.colnames = FALSE,
