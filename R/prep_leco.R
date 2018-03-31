39718e705193b1dff258a71cf70dd2688fdf3d5f
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-31 17:34:06 +1100

    add prep_leco roxygen

--- R/prep_leco.R
+++ R/prep_leco.R
@@ -1,7 +1,17 @@
+#' Prepare C and N trait data
+#'
+#' @param leco_data raw leco data
+#' @return prepared leco data
+#'
+#' @export
+
 prep_leco <- function (leco_data) {
   
+  # read leco data
   leco <- read.csv(leco_data, header = F, skip = 12)
-  leco <- leco[!(leco[,1] == 'donotuse' | leco[,1] == 'AA'), c(1,6,7)]
+  
+  # exclude malformed sample and unused species. select columns for C and N
+  leco <- leco[!(leco[, 1] == 'donotuse' | leco[, 1] == 'AA'), c(1,6,7)]
   colnames(leco) <- c('species_code', 'C', 'N')
   leco
   
